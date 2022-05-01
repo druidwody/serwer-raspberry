bda0298d3d0184342fe46dc5a9765785229b16dd
druidwody <itahi4@gmail.com>
2022-05-01 21:32:47 +0200

    cos

---
+++ serwer.js
@@ -0,0 +1,4 @@
+//tablica z kulinarnymi angielskimi wyrazami
+let kulinarne = new Array("A la carte","Al dente","A la grecque","A point","Acidulation","Aerate","Aspic","Au gratin","Au jus","Au poivre","Au sec","Bain Marie","Barding","Baste","Beurre blanc","Bisque","Blanching","Braising","Brining","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",);
+
+console.log(kulinarne);
