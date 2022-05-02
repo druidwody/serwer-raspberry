b8a40a28b6bf527c74f2f454e14bd5cf19d9a725
druidwody <itahi4@gmail.com>
2022-05-02 20:43:16 +0200

    Commit message

--- serwer.js
+++ serwer.js
@@ -1,4 +1,4 @@
 //tablica z kulinarnymi angielskimi wyrazami
-let kulinarne = new Array("A la carte","Al dente","A la grecque","A point","Acidulation","Aerate","Aspic","Au gratin","Au jus","Au poivre","Au sec","Bain Marie","Barding","Baste","Beurre blanc","Bisque","Blanching","Braising","Brining","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",);
+let kulinarne = new Array("A la carte","Al dente","A la grecque","A point","Acidulation","Aerate","Aspic","Au gratin","Au jus","Au poivre","Au sec","Bain Marie","Barding","Baste","Beurre blanc","Bisque","Blanching","Braising","Brining","Chiffonade","Concasse","Consommé","Confit","Coring","Coulis","Croquette","Deglaze","Degrease","Dredging","Dress","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",);
 
 console.log(kulinarne);
